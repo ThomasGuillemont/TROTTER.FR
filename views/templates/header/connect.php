90efefbf00873967c0dd888333c5e987865d53b1
Thomas Guillemont <thomas.guillemont@hotmail.fr>
2022-05-12 11:26:31 +0200

    V 0.4.3

--- views/templates/header/connect.php
+++ views/templates/header/connect.php
@@ -6,10 +6,10 @@
                 <a class="nav-link active" href="/accueil">Accueil</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link active" href="/profil?id=<?= $_SESSION['user']->id ?? '' ?>">Mon profil</a>
+                <a class="nav-link" href="/actualités">Fil d'actualités</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="/actualités">Fil d'actualités</a>
+                <a class="nav-link active" href="/profil?id=<?= $_SESSION['user']->id ?? '' ?>">Mon profil</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="/amis?id=<?= $_SESSION['user']->id ?? '' ?>">Mes amis</a>
